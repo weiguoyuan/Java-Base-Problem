09dcc4d24546c7ffc58d4ed0dc45d66ef634c477
weiguoyuan <weiguoyuan@163.com>
2017-10-23 12:10:05 +0800

    add note

--- src/weiguoyuan/Concurrency/TestAtomic.java
+++ src/weiguoyuan/Concurrency/TestAtomic.java
@@ -4,11 +4,16 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by william on 2017/10/16.
+ * 测试导致多线程出错的问题,数据竞争和竞争条件
+ * 测试运算的原子性
+ * 数据竞争和竞争条件的关系 http://www.jianshu.com/p/dd82d070e5c1
+ * java.util.concurrent.atomic可以解决原子性问题(数据竞争问题),如i=i+1(incrmentAndGet()替代);的操作,
+ * 但竞态条件问题还是要锁来解决.
  */
 
 class Atomic implements Runnable{
 
-    private final AtomicLong count = new AtomicLong(1000);
+    private final AtomicLong count = new AtomicLong(1000);//java的原子运算
 
     @Override
     public void run() {
