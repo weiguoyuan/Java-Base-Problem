6f7c34573f9967874544bc107193150195557abb
weiguoyuan <weiguoyuan@163.com>
2017-10-24 14:53:13 +0800

    modify ThreadLocal

--- src/weiguoyuan/Concurrency/TestThreadLocal.java
+++ src/weiguoyuan/Concurrency/TestThreadLocal.java
@@ -27,11 +27,11 @@ public class TestThreadLocal {
         }
 
         public void run() {
-            System.out.println("线程" + index + "的初始value:" + value.get());
+            System.out.println("Thread" + index + "\'s init value:" + value.get());
             for (int i = 0; i < 10; i++) {
                 value.set(value.get() + index);
             }
-            System.out.println("线程" + index + "的累加value:" + value.get());
+            System.out.println("Thread" + index + "\'s accumulation value:" + value.get());
         }
     }
 }
