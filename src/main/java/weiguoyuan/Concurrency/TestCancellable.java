6f263683c13f017d338831e092f3d7fbe48de111
weiguoyuan <weiguoyuan@163.com>
2017-11-16 10:31:48 +0800

    test canclle thread

---
+++ src/main/java/weiguoyuan/Concurrency/TestCancellable.java
@@ -0,0 +1,62 @@
+package weiguoyuan.Concurrency;
+
+import org.apache.http.annotation.GuardedBy;
+import org.apache.http.annotation.ThreadSafe;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by william on 2017/11/15.
+ * 测试线程的取消 输出1以后的素数 1s后终止
+ */
+
+public class TestCancellable{
+
+    public static void main(String[] args){
+        try {
+            aSecondOfPrimes();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<BigInteger> aSecondOfPrimes()throws InterruptedException{
+        PrimeGenerator generator = new PrimeGenerator();
+        new Thread(generator).start();
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        }finally {
+            generator.cancel();
+        }
+        return generator.get();
+    }
+}
+@ThreadSafe
+class PrimeGenerator implements Runnable {
+    @GuardedBy("this")
+    private final List<BigInteger> primes = new ArrayList<BigInteger>();
+    private volatile boolean cancelled;
+
+    public void run(){
+        BigInteger p = BigInteger.ONE;
+        while (!cancelled){
+            p = p.nextProbablePrime();
+            System.out.println(p);
+            synchronized (this){
+                primes.add(p);
+            }
+        }
+    }
+
+    public void cancel(){
+        cancelled = true;
+    }
+
+    public  synchronized List<BigInteger> get(){
+        return new ArrayList<BigInteger>(primes);
+    }
+
+}
