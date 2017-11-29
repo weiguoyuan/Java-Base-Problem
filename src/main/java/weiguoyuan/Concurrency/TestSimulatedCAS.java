8a51847e40f863180124eecf822d65286ce8feae
weiguoyuan <weiguoyuan@163.com>
2017-11-29 14:44:03 +0800

    add test CAS

--- src/main/java/weiguoyuan/Concurrency/TestSimulatedCAS.java
+++ src/main/java/weiguoyuan/Concurrency/TestSimulatedCAS.java
@@ -8,7 +8,7 @@ import org.apache.http.annotation.ThreadSafe;
  * CAS(在大多数处理器架构中实现的比较交换指令) CAS包括3个操作数-需要读写的内存位置V 进行比较的值A 要更新为值B
  * 当V==A CAS才会通过原子方式用新值B更新V并返回V的值 否则不修改返回V的值 (和数据库版本号的乐观锁相似)
  * 多个线程尝试使用CAS同时更新一个变量时 只有一个能更新变量的值 其他的线程都将失败 虽然失败但不会像锁或同步代码块会阻塞线程
- * 线程执行失败后 可根据业务 返回错误提示等 所以CAS能不使用锁实现原子的读-改-写操作
+ * 线程执行失败后 可根据业务 返回错误提示等 所以CAS能不使用(阻塞)锁实现原子的读-改-写操作
  */
 @ThreadSafe
 public class TestSimulatedCAS {
@@ -16,7 +16,7 @@ public class TestSimulatedCAS {
 
     public synchronized int get(){return value;}//加锁模拟CAS
 
-    public synchronized int compareAndSwao(int expectedValue,int newValue){
+    public synchronized int compareAndSwap(int expectedValue,int newValue){
         int oldValue = value;
         if(oldValue ==expectedValue){
             value = newValue;
@@ -25,6 +25,6 @@ public class TestSimulatedCAS {
     }
 
     public synchronized boolean compareAndSet(int expectedValue,int newValue){
-        return (expectedValue == compareAndSwao(expectedValue,newValue));
+        return (expectedValue == compareAndSwap(expectedValue,newValue));
     }
 }
