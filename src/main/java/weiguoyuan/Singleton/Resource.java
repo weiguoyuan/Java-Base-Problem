fb32ab47078fcfe325c08ae2eb2205ac41f76566
weiguoyuan <weiguoyuan@163.com>
2017-12-25 13:12:51 +0800

    add singleton

---
+++ src/main/java/weiguoyuan/Singleton/Resource.java
@@ -0,0 +1,8 @@
+package weiguoyuan.Singleton;
+
+/**
+ * Created by william on 2017/12/25.
+ * resource
+ */
+public class Resource {
+}
---
+++ src/main/java/weiguoyuan/Singleton/Singleton.java
@@ -0,0 +1,69 @@
+package weiguoyuan.Singleton;
+
+import org.apache.http.annotation.ThreadSafe;
+
+/**
+ * Created by william on 2017/12/25.
+ * 正确的单例和错误的单例
+ */
+public class Singleton {
+}
+
+class WrongSingleton {
+
+    private static volatile Resource resource = null;
+
+    public static Resource getInstance() {
+        if (resource == null) { //volatile保证变量对线程的可见性，但不保证原子性。并发下会多次执行新建对象
+            resource = new Resource();
+        }
+        return resource;
+    }
+}
+
+class WrongSingleton2 {//双重检查加锁(DCL) 也是错误的 当缺少Happens-Before关系时,就可能出现重排序问题,
+                       //这就解释了为什么没有充分同步的情况下发布一个对象会导致另一个线程看到一个只部分构造的对象
+    private static Resource resource = null;
+
+    public static Resource getInstance() {
+        if (resource == null) { //有可能其他线程新建了对象 resource的地址不为空 但是这个对象还没有构造完成 应该用volatile 标注resource
+            synchronized (WrongSingleton.class) {
+                if (resource == null)
+                    resource = new Resource();
+            }
+        }
+        return resource;
+    }
+}
+
+@ThreadSafe
+class SafeLazyInitialization {
+
+    private static Resource resource = null;
+
+    public synchronized static Resource getInstance() {
+        if (resource == null)
+            resource = new Resource();
+        return resource;
+    }
+}
+
+@ThreadSafe
+class EagerInitialization {//提前初始化
+
+    private static Resource resource = new Resource();
+
+    public static Resource getResource() { return resource; }
+}
+
+@ThreadSafe
+class ResourceFactory {//延迟初始化占位类模式 推荐
+    private static class ResourceHolder {
+        public static Resource resource = new Resource();
+    }
+    public static Resource getResource() {
+        return ResourceHolder.resource ;
+    }
+}
+
+
