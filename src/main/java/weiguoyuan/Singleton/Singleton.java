f5b6567a3aa7df663a4e0e9f03897ce5ff4974a1
weiguoyuan <weiguoyuan@163.com>
2017-12-26 15:47:50 +0800

    add note

--- src/main/java/weiguoyuan/Singleton/Singleton.java
+++ src/main/java/weiguoyuan/Singleton/Singleton.java
@@ -25,7 +25,7 @@ class WrongSingleton2 {//双重检查加锁(DCL) 也是错误的 当缺少Happen
                        //这就解释了为什么没有充分同步的情况下发布一个对象会导致另一个线程看到一个只部分构造的对象
     private static Resource resource = null;
 
-    public static Resource getInstance() {
+    public static Resource getInstance() {//这么设计当初是为了解决两个问题 1无竞争同步的执行速度很慢(synchronized锁当时没有偏向锁和轻量锁) 2JVM启动很慢(懒加载写法)
         if (resource == null) { //有可能其他线程新建了对象 resource的地址不为空 但是这个对象还没有构造完成 应该用volatile 标注resource
             synchronized (WrongSingleton.class) {
                 if (resource == null)
@@ -58,7 +58,7 @@ class EagerInitialization {//提前初始化
 
 @ThreadSafe
 class ResourceFactory {//延迟初始化占位类模式 推荐
-    private static class ResourceHolder {
+    private static class ResourceHolder {//内部类用法
         public static Resource resource = new Resource();
     }
     public static Resource getResource() {
